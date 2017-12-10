418ff18e2f09dd9d37c2733683c2b7ee5bc6a6f7
hylllllll <hanyulin218@163.com>
2017-12-10 16:14:15 +0800

    完成注册功能

--- df_user/urls.py
+++ df_user/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from df_user import views
 urlpatterns = [
     url(r'^register/$', views.register, name='register'),
+    url(r'^register_handle/$', views.register_handle, name='register_handle'),
 ]
--- df_user/views.py
+++ df_user/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from hashlib import sha1
+from .models import UserInfo
 
 
 def register(request):
     """注册"""
     return render(request, 'df_user/register.html')
+
+
+def register_handle(request):
+    """注册信息"""
+    # 接收用户输入
+    post = request.POST
+    uname = post.get('user_name')
+    upwd = post.get('pwd')
+    upwd2 = post.get('cpwd')
+    uemail = post.get('email')
+    # 判断两次密码是否一致
+    if upwd!=upwd2:
+        # 密码不一致，重定向到注册页
+        return redirect('user/register/')
+    else:
+        # 加密密码
+        s1 = sha1()
+        s1.update(upwd.encode('utf-8'))
+        upwd3 = s1.hexdigest()
+        # 创建模型对象,添加数据
+        user = UserInfo()
+        user.uname = uname
+        user.upwd = upwd3
+        user.uemail = uemail
+        # 密码一致，重定向到登录页面
+        return redirect('/user/login/')
